74286ca4539d17baff0b143bd898c65ffe4a4407
not-known-person <ayushnigam843@gmail.com>
2024-07-26 12:35:43 +0530

    Added Post Template

--- project/project/views.py
+++ project/project/views.py
@@ -1,18 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from categories.models import Categories
+from posts.models import Post
 def home(request):
     categories = Categories.objects.all()
     props = {
         "categories" : categories
     }
+    
     print(categories)
     return render(request , "home.html",props)
 def auth(request):
     if request.method == 'POST':
         pass
     return render(request , "auth.html")
-def getcategories(request):
-    categories = Categories.objects.get()
-    return
+
+def posts(request):
+    if request.method == 'POST':
+        pass
+    
+    category = request.GET.get("category")
+    print(category)
+    category = Categories.objects.get(name = category)
+    posts = Post.objects.all().filter(category = category)
+    # print(posts)
+    props={
+        "posts":posts
+    }
+    return render(request , "posts.html",props)
     
