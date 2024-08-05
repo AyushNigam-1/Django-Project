cc940abd2172a4b3030120304f2eff6b29fc608f
Ayush <ayushnigam843@gmail.com>
2024-08-05 20:51:59 +0530

    Fixed Models Fields

--- project/company/models.py
+++ project/company/models.py
@@ -3,7 +3,7 @@ class Company(models.Model):
     name = models.CharField(max_length=2000)
     location = models.CharField(max_length=2000)
     desc = models.CharField(max_length=10000)
-    posts = models.ForeignKey("posts.Post", on_delete=models.CASCADE)
+    posts = models.ForeignKey("posts.Post", on_delete=models.CASCADE , related_name='posts')
     phone_number = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
--- project/posts/models.py
+++ project/posts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title=models.CharField(max_length=200)
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
-    company = models.ForeignKey("company.Company", on_delete=models.CASCADE)
+    company = models.ForeignKey("company.Company", on_delete=models.CASCADE,related_name='company_posts')
     applicants = models.ManyToManyField(User, related_name='users',blank=True)
     location = models.CharField(max_length=2000)
     desc = models.CharField(max_length=10000)
