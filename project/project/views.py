baa0eea2823f5b44e39b20bd05c0c79b75eb9630
Ayush <ayushnigam843@gmail.com>
2024-08-18 20:53:32 +0530

    Added Company Profile Page

--- project/project/views.py
+++ project/project/views.py
@@ -87,25 +87,6 @@ def register(request):
     
     return render(request,'register.html')
 
-def register(request):
-    if request.method == 'POST': 
-        first_name = request.POST.get("first_name")
-        last_name = request.POST.get("last_name")
-        username = request.POST.get("username")
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        print(first_name)
-        if User.objects.filter(email = email).exists():
-            messages.error(request , 'Please try another email')
-            return render(request , 'register.html')
-
-        user = User(first_name=first_name,last_name=last_name,username=username, email=email , password = make_password(password))
-        user.save()
-        login(request,user=user)
-        return redirect('home')
-    
-    return render(request,'register.html')
-
 def logout_user(request):
     logout(request)
     return redirect('home')
@@ -203,8 +184,10 @@ def profile(request):
 def company(request):
     id = request.GET.get("id")
     company = Company.objects.get(id=id)
+    posts = company.posts.all()
     props = {
         "company": company,
+        "posts":posts
     }
     print(props)
     return render(request , "company.html" ,props)
